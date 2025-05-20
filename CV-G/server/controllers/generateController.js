1753b8b91650cee2e240b5919914642bbf2ecbba
MIF20009 <mostafa-fa@outlook.com>
2025-05-20 14:18:19 +0300

    install packages #2

--- CV-G/server/controllers/generateController.js
+++ CV-G/server/controllers/generateController.js
@@ -228,7 +228,11 @@ ${userInfo}
       </html>
     `;
 
-    const browser = await puppeteer.launch({ headless: 'new' });
+    // const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
     const pdfBuffer = await page.pdf({ format: 'A4', printBackground: false });
