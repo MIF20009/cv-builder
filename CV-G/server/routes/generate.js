50567003ce4bd7673e2117e03d32c6bd19f7ba2e
MIF20009 <mostafa-fa@outlook.com>
2025-05-29 16:28:32 +0300

    fix import auth in gen route

--- CV-G/server/routes/generate.js
+++ CV-G/server/routes/generate.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { generateCV } = require('../controllers/generateController');
-const authenticate = require('../middleware/authenticate');
+const authenticate = require('../middleware/authMiddleware');
 
 
 router.post('/',authenticate, generateCV)
