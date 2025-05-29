2dd192c6ac3fc085fb786786566ee329e68e2be8
MIF20009 <mostafa-fa@outlook.com>
2025-05-30 02:10:24 +0300

    fix dashboard #4

--- CV-G/server/controllers/dashboardController.js
+++ CV-G/server/controllers/dashboardController.js
@@ -1,7 +1,3 @@
-const {User} = require('../models/User'); // assuming Mongoose model
-const {Education} = require('../models/Education');
-const {Experience} = require('../models/Experience');
-
 const getUserDashboard = async (req, res) => {
   try {
     const userId = req.user.id;
@@ -10,20 +6,17 @@ const getUserDashboard = async (req, res) => {
     const user = await User.findByPk(userId, { attributes: { exclude: ['password'] } });
     if (!user) return res.status(404).json({ message: 'User not found' });
 
-    const education = await Education.findAll({ where: { userId } });
-    const experience = await Experience.findAll({ where: { userId } });
+    const education = await Education.findAll({ where: { user_id: userId } });
+    const experience = await Experience.findAll({ where: { user_id: userId } });
 
     res.json({
       user,
       education,
       experience,
-      cvUrl: user.cvUrl
+      cvUrl: user.cv_url  // snake_case here
     });
   } catch (err) {
     console.error("Error in getUserDashboard:", err);
     res.status(500).json({ message: 'Server error', error: err.message });
   }
 };
-
-
-module.exports = { getUserDashboard };
